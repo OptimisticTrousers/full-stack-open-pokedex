0607a4db503d3e03250acbf4fa6a0d39cb4a796b
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-07-06 20:14:28 -0400

    Complete Exercise 11.9 Simple end-to-end-tests.

---
+++ e2e-tests/pokedex.spec.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-undef
+const { test, describe, expect } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(
+      page.getByText(
+        'Pokémon and Pokémon character names are trademarks of Nintendo.'
+      )
+    ).toBeVisible()
+  })
+  test('pokemon detail page can be opened', async ({ page }) => {
+    await page.goto('')
+    await page.getByRole('link', { name: 'ivysaur' }).click()
+
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
