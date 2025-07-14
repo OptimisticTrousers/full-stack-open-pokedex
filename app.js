e4311370da585263a0f2d6f17eaf46dd89796b7b
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-07-14 07:54:39 -0400

    Complete Exercise 11.12 Health check (Fly.io).

--- app.js
+++ app.js
@@ -16,3 +16,9 @@ app.listen(PORT, () => {
 app.get('/version', (req, res) => {
   res.send('1')
 })
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw ('error...')
+  res.send('ok')
+})
